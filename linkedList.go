608d538acf7b3b3b42ab2c0ef4d3bce840e75215
Joepolymath <joshuaajagbe96@gmail.com>
2023-01-13 15:29:12 +0100

    insert and isEmpty methods

--- linkedList.go
+++ linkedList.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
+// IMPLEMENTING LINKED LIST IN GOLANG...
 type Node struct {
 	value int
 	next *Node
@@ -10,6 +13,37 @@ type LinkedList struct {
 	len int
 }
 
+func (l *LinkedList) isEmpty() bool {
+	return l.len == 0
+}
+
+func (l *LinkedList) insert(val int) Node {
+	n := Node{}
+	n.value = val
+
+	if l.len == 0 {
+		l.head = &n
+		l.len++
+		return n
+	}
+	currentHead := l.head
+	for i:= 0; i < l.len; i++ {
+		if currentHead.next == nil {
+			currentHead.next = &n
+			l.len++
+			return n
+		}
+		currentHead = currentHead.next
+	}
+	return n
+}
+
 func main()  {
-	
+	list := LinkedList{}
+	fmt.Println(list.isEmpty())
+	fmt.Println(list)
+	list.insert(5)
+	list.insert(6)
+	fmt.Println(list)
+	fmt.Println(list.isEmpty())
 }
